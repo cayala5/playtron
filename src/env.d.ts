919aeafba8b730e467a757599c9219152ed3f267
cayala5 <webstersprophet@gmail.com>
2024-01-26 17:30:44 -0500

    Authenticate user with spotify API (#1)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+/* CHRISTIAN TODO: implement cookie checking via middleware, and set session on locals
+declare namespace App {
+    interface Locals {
+        error: Error;
+        errorObj: any;
+    }
+}
+*/
